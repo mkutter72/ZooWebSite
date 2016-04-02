4e5df63201c9294d43a1deb1fe6e845fb6462abd
MikeKutter <mkutter@yahoo.com>
2016-04-02 11:00:47 -0400

    commit

--- postcard.js
+++ postcard.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : 1105905996137106,  /*'1105769736150732', */
+      appId      : 1105769736150732,  /*'', 1105905996137106*/
       xfbml      : true,
        version    : 'v2.5'
     });
