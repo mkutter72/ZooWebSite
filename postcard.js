c02e695e3fd8888c79b96580bb050a08729f1e0e
MikeKutter <mkutter@yahoo.com>
2016-04-02 10:04:44 -0400

    commit

--- postcard.js
+++ postcard.js
@@ -2,6 +2,26 @@
 
 $(document).ready(function () {
 
+
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : 1105905996137106,  /*'1105769736150732', */
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    };
+
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+
+
+
   $('#facebookLogin').on('click',function (e){
     e.preventDefault();
     console.log("Logging in");
