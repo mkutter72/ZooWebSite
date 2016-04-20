8ae2e0043f97714de64473fa0f63801be76c3370
MikeKutter <mkutter@yahoo.com>
2016-04-19 21:50:16 -0400

    commit

--- postcard.js
+++ postcard.js
@@ -180,15 +180,15 @@ $(document).ready(function () {
         //     }
         //   });
 
-       twitter.post('/1.1/media/upload.json', {
-          data: {
-             media: './postcard.png'
-             }
-           });
+       // twitter.post('/1.1/media/upload.json', {
+       //    data: {
+       //       media: './postcard.png'
+       //       }
+       //     });
 
        twitter.post('https://upload.twitter.com/1.1/media/upload.json', {
           data: {
-             media: './postcard.png'
+             media: 'postcard.png'
              }
            });
 
